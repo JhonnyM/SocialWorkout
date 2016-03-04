df4db6025dc7954d5260f2fa8622bb71f1beeac2
Jhonny <killingjokewow@gmail.com>
2016-03-03 21:45:29 -0600

    Adding Modals to edit objetivos

--- src/main/webapp/resources/objetivos/objetivos.js
+++ src/main/webapp/resources/objetivos/objetivos.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.objetivos', ['ngRoute'])
+angular.module('myApp.objetivos', ['ngRoute','ui.grid', 'ui.bootstrap'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/objetivos', {
@@ -9,7 +9,7 @@ angular.module('myApp.objetivos', ['ngRoute'])
   });
 }])
 
-.controller('ObjetivosCtrl', ['$scope','$http',function($scope,$http) {
+.controller('ObjetivosCtrl', ['$scope','$http','$uibModal', function($scope,$http,$uibModal) {
 	$scope.objetivos = [];
 	$scope.requestObject = {"pageNumber": 0,"pageSize": 0, "sortBy": [""],"searchColumn": "string","searchTerm": "","objetivo": {}};
 	$http.post('rest/protected/objetivos/getAll',$scope.requestObject).success(function(response) {
@@ -18,6 +18,34 @@ angular.module('myApp.objetivos', ['ngRoute'])
 	console.log("$scope.objetivos",$scope.objetivos)
 		
 	});
+
+    $scope.gridOptions = {
+        data:'objetivos',
+        showGroupPanel: true,
+        enableSorting:true,
+        enableFiltering:true,
+        columnDefs:[
+            {field:'idObjetivo',displayName:'ID'},
+            {field:'descObjetivo',displayName:'Descripción'},
+            {field:'Acciones', displayName:'Acciones',cellTemplate: '<p ng-click="grid.appScope.editRow(row)">Edit</p>'} 
+        ]
+    };
+
+    $scope.editRow = function(row){
+       console.log("me dieron click",row.entity);
+       var dialogOpts = {
+           backdrop:'static',
+           keyboard:false,
+           templateUrl:'resources/objetivos/edit-modal.html',
+           controller:'ObjetivosModal',
+           size:"lg",
+           windowClass:"modal",
+           resolve:{
+               objetivo:function(){return row.entity}
+           }
+       };
+       $uibModal.open(dialogOpts)
+    };
 
 	$scope.saveObjetivo = function(event){
     	
