f216cb07b53e8ac10679470b691e3f46943e2541
Jhonny <killingjokewow@gmail.com>
2016-03-04 00:52:21 -0600

    Common:Adding files for modals

--- src/main/webapp/resources/app.js
+++ src/main/webapp/resources/app.js
@@ -6,8 +6,9 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.version',
+  'myApp.modal',
   'ui.grid',
-  'angularFileUpload'
+  'angularFileUpload',
 ])
 .config(['$routeProvider','$provide','$httpProvider', function($routeProvider,$provide,$httpProvider) {
 	$routeProvider.otherwise({redirectTo: '/view1'});
