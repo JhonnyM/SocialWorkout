927c626eaab2b2c8bac6462d4191a01e24adf14e
joarix3 <biskuiarias@gmail.com>
2016-04-09 16:32:36 +0200

    Fix problema pojo que no cargo.

---
+++ src/main/java/com/cenfotec/socialWorkout/pojo/RutinaHasUsuarioPOJO.java
@@ -0,0 +1,91 @@
+
+package com.cenfotec.socialWorkout.pojo;
+
+import java.sql.Time;
+import java.util.Date;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import com.cenfotec.socialWorkout.ejb.Plantillarutinamaestro;
+import com.cenfotec.socialWorkout.ejb.Usuario;
+
+public class RutinaHasUsuarioPOJO {
+
+
+	private int idRegistroRutinaXUsuario;
+	private String diaSemanalAgendado;
+	private Date fechaInicio;
+	private Date fechaVencimiento;
+	private Time hora;
+	private Plantillarutinamaestro plantillarutinamaestro;
+	private Usuario usuario;
+
+	
+	public RutinaHasUsuarioPOJO() {
+		super();
+	}
+
+
+	public int getIdRegistroRutinaXUsuario() {
+		return idRegistroRutinaXUsuario;
+	}
+
+
+	public void setIdRegistroRutinaXUsuario(int idRegistroRutinaXUsuario) {
+		this.idRegistroRutinaXUsuario = idRegistroRutinaXUsuario;
+	}
+
+
+	public String getDiaSemanalAgendado() {
+		return diaSemanalAgendado;
+	}
+
+
+	public void setDiaSemanalAgendado(String diaSemanalAgendado) {
+		this.diaSemanalAgendado = diaSemanalAgendado;
+	}
+
+
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+
+	public Date getFechaVencimiento() {
+		return fechaVencimiento;
+	}
+
+
+	public void setFechaVencimiento(Date fechaVencimiento) {
+		this.fechaVencimiento = fechaVencimiento;
+	}
+
+
+	public Time getHora() {
+		return hora;
+	}
+
+
+	public void setHora(Time hora) {
+		this.hora = hora;
+	}
+
+
+	public Plantillarutinamaestro getPlantillarutinamaestro() {
+		return plantillarutinamaestro;
+	}
+
+
+	public void setPlantillarutinamaestro(Plantillarutinamaestro plantillarutinamaestro) {
+		this.plantillarutinamaestro = plantillarutinamaestro;
+	}
+
+}
