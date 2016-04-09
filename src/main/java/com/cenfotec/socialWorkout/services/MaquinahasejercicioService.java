e215f21899493c0067d0616f42bc2d948ebb3edc
Jhonny <killingjokewow@gmail.com>
2016-04-09 12:12:47 -0600

    Editar Rutinas add backend for relation

--- src/main/java/com/cenfotec/socialWorkout/services/MaquinahasejercicioService.java
+++ src/main/java/com/cenfotec/socialWorkout/services/MaquinahasejercicioService.java
@@ -1,13 +1,17 @@
 package com.cenfotec.socialWorkout.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cenfotec.socialWorkout.contracts.MaquinahasejercicioRequest;
 import com.cenfotec.socialWorkout.ejb.Maquinahasejercicio;
-import com.cenfotec.socialWorkout.ejb.Unidadmedida;
+import com.cenfotec.socialWorkout.pojo.MaquinahasejercicioPOJO;
 import com.cenfotec.socialWorkout.repositories.MaquinahasejercicioRepository;
 
 @Service
@@ -16,6 +20,39 @@ public class MaquinahasejercicioService implements MaquinahasejercicioServiceInt
 	@Autowired
 	private MaquinahasejercicioRepository maquinaHasEjercicioRepository;
 
+	@Override
+	@Transactional
+	public List<MaquinahasejercicioPOJO> getAll() {		
+		List<Maquinahasejercicio> relationList = maquinaHasEjercicioRepository.findAll();
+		List<MaquinahasejercicioPOJO> dtos = new ArrayList<MaquinahasejercicioPOJO>();
+		relationList.stream().forEach(ta ->{
+			MaquinahasejercicioPOJO dto = new MaquinahasejercicioPOJO();
+			BeanUtils.copyProperties(ta, dto);
+			dtos.add(dto);
+			
+		});
+		return dtos;	
+	}
 
+	@Override
+	public boolean save (MaquinahasejercicioRequest request){
+		MaquinahasejercicioPOJO eventoDTO = request.getMaquinaEjercicio();
+		Maquinahasejercicio relation = new Maquinahasejercicio();
+		BeanUtils.copyProperties(eventoDTO, relation);
+		Maquinahasejercicio s = maquinaHasEjercicioRepository.save(relation);
+		return !(s == null);
+		
+	}
+	
+	@Override
+	public boolean exists (Integer idRelation){		
+		return maquinaHasEjercicioRepository.exists(idRelation);		
+	}
+
+	@Override
+	public boolean delete(int idRelation) {	
+		maquinaHasEjercicioRepository.delete(idRelation);
+		return !maquinaHasEjercicioRepository.exists(idRelation);
+	}
 
 }
--- src/main/java/com/cenfotec/socialWorkout/services/MaquinahasejercicioServiceInterface.java
+++ src/main/java/com/cenfotec/socialWorkout/services/MaquinahasejercicioServiceInterface.java
@@ -2,9 +2,16 @@ package com.cenfotec.socialWorkout.services;
 
 import java.util.List;
 
+import com.cenfotec.socialWorkout.contracts.MaquinahasejercicioRequest;
 import com.cenfotec.socialWorkout.ejb.Maquinahasejercicio;
+import com.cenfotec.socialWorkout.pojo.MaquinahasejercicioPOJO;
 
 public interface MaquinahasejercicioServiceInterface {
+
+	public List<MaquinahasejercicioPOJO> getAll();
+	public boolean save (MaquinahasejercicioRequest request);
+	public boolean delete(int idTipo);
+	public boolean exists (Integer idTipo);
 
 	
 }
