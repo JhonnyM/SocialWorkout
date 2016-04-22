cd051c40c7b872b476fc37ee24e574dd119132d3
Jhonny <killingjokewow@gmail.com>
2016-04-22 01:46:20 -0600

    Add validations to the eventos crud

--- src/main/webapp/resources/eventos/eventosModal.js
+++ src/main/webapp/resources/eventos/eventosModal.js
@@ -2,20 +2,48 @@
 
 angular.module('myApp.eventosModal', ['ngRoute', 'ui.grid', 'schemaForm', 'ui.bootstrap'])
 
-.controller('EventosModalCtrl', ['$scope','$http','$uibModalInstance','evento',  function($scope,$http,$uibModalInstance,evento)
+.controller('EventosModalCtrl', ['$scope','$http','$uibModalInstance','evento','$route',  function($scope,$http,$uibModalInstance,evento, $route)
 {
+	$scope.reload = function() {
+    	$route.reload();
+  	};
 
 	$scope.EventoSchema = {
 	  "type": "object",
 	  properties: {
-		  descEvento: { type: 'string', title: 'Descripción'},
-		  fechaHoraInicio: { title: 'Fecha de Inicio'},
-		  fechaHoraFinal: {  title: 'Fecha Final'},
-		  observaciones: { type: 'string', title: 'Observaciones'}
-	  	}
+		  descEvento: { 
+	  					type: 'string', 
+	  					title: 'Descripción', 
+	  					validationMessage: 'Descripción de evento inválido',
+	                	pattern: "^[A-Za-z áéíóú.!=/-]+$", 
+	                	maxLength: 50
+		            },
+		  fechaHoraInicio: { 
+
+		  					title: 'Fecha de Inicio'
+		  				},
+		  fechaHoraFinal: {  
+
+		  					title: 'Fecha Final'
+		  				},
+		  observaciones: { 
+
+	  					   	type: 'string', 
+	  					   	title: 'Observaciones',
+	  					   	validationMessage: 'Observaciones de evento inválido',
+		                   	pattern: "^[A-Za-z áéíóú.!=/-]+$", 
+		                	maxLength: 50
+		  				}
+	  	},
+	  	required : ['descEvento', 'fechaHoraInicio', 'fechaHoraFinal', 'observaciones' ]
 	};
     
 	$scope.eventoForm = angular.copy(evento);
+
+
+	$scope.reload = function(){
+    	$route.reload();
+  	};
 	
     $scope.form = [
 		'descEvento',
@@ -37,18 +65,25 @@ angular.module('myApp.eventosModal', ['ngRoute', 'ui.grid', 'schemaForm', 'ui.bo
 			fechaHoraFinal: $scope.eventoForm.fechaHoraFinal,
 			observaciones: $scope.eventoForm.observaciones
 	  	};
-		$http.post('rest/protected/eventos/update',{evento: data})
-		.then(function (response){
-	      switch(response.data.code)
-	      {
-	        case 200:
-	          alert(response.data.codeMessage);
-	        break;
-	        default:
-	          alert(response.data.codeMessage);
-	      }
-	    }, function (response){
-	      alert("Error al guardar la información de el nuevo evento");
-	    });
+	  	$scope.valid = tv4.validate($scope.eventoForm, $scope.EventoSchema);
+			if($scope.valid){
+				$http.post('rest/protected/eventos/update',{evento: data})
+				.then(function (response){
+			      switch(response.data.code)
+			      {
+			        case 200:
+			          alert(response.data.codeMessage);
+			          $scope.dismissModal = $scope.reload();
+			        break;
+			        default:
+			          alert(response.data.codeMessage);
+			          $scope.dismissModal = $scope.reload();
+			      }
+			    }, function (response){
+			      alert("Error al guardar la información de el nuevo evento");
+			    });
+			    $uibModalInstance.close();
+		} 
+		$scope.valid = false;
 	};
 }]);
